3b3cbc9ed907c6cab5f5b94477cf93b6fdb92dac
Ekupov,Velin V.S <v.ekupov@student.fontys.nl>
2018-04-19 20:31:14 +0200

    added calendar ot tickets

--- res/js/stickynav.js
+++ res/js/stickynav.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#stickypoint').waypoint(function(direction) {
         if(direction == 'down') {
             $('#home-nav').addClass('no-display');
-            $('#main-nav').addClass('fixed-top scrolling-nav');
+            $('#main-nav').removeClass('no-display');
             $('#main-nav').slideDown(150);
         } else if(direction == 'up') {
             $('#home-nav').removeClass('no-display');
