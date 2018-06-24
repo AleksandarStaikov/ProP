5640055e585294eeabb94fde055d6db63a0c8ca8
AleksandarStaikov <apsreat@gmail.com>
2018-06-24 21:59:59 +0200

    shopping app refactored

---
+++ apps/LoaningApplicationProP/LoaningApplicationProP/Models/Item.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace LoaningApplicationProP.Models
+{
+    enum Sizes { Small, Medium, Big };
+
+    [Serializable]
+    public class Item
+    {
+        public Item(long id, string name, double price, string available, Image img)
+        {
+            this.SellingPrice = price;
+            this.Name = name;
+            this.Quantity = 1;
+            this.ItemID = id;
+            this.ItemImage = img;
+            this.Available = available;
+        }
+
+        public double SellingPrice { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public long ItemID { get; set; }
+
+        public Image ItemImage { get; set; }
+
+        public string Available { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Name}, {ItemID}";
+        }
+    }
+}
---
+++ apps/LoaningApplicationProP/LoaningApplicationProP/Models/Tent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoaningApplicationProP.Models
+{
+    public class Tent
+    {
+        public Tent(short size, int id)
+        {
+            this.Id = id;
+            this.size = size;
+        }
+
+        public short size { get; set; }
+
+        public int  Id { get; set; }
+    }
+}
